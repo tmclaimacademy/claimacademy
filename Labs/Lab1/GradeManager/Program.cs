b39837f4c9cce444bdd2da31e50a7bad148449d7
Tavish Misra <tmclaimacademy@gmail.com>
2024-07-12 20:03:21 -0500

    Started menu feature

--- Labs/Lab1/GradeManager/Program.cs
+++ Labs/Lab1/GradeManager/Program.cs
@@ -10,7 +10,11 @@ namespace GradeManager
     {
         public static void Main(string[] args)
         {
+            string applicationName = "Grade Manager"; //Declare application name as a string
 
+            Console.WriteLine(applicationName); //Print application name on first line
+            Console.WriteLine(new String('-', applicationName.Length)); //Print line on name equal to length of application name. This is a dynamically-built string. We are creating a String object, calling the String class constructor with the new keyword. It is accepting two parameters, the first is a character to print, the second is an integer representing the count of characters to build the string. The Length property on the applicationName string gives us the integer count of applicationName so the count of dashes matches the length of the title.
+            Console.ReadKey();
         }
     }
 }
