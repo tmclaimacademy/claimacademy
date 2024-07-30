90d4809bed351f4e113fd6757d056ea0bde485dd
Tavish Misra <tmclaimacademy@gmail.com>
2024-07-29 20:14:52 -0500

    Created Practice Exercises project with first problems

---
+++ PracticeExercises/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercises
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             * Practice Exercises - 7/29/2024
+             * ------------------------------
+             * 
+             * 1. Write a C# Sharp program to print the results of the specified operations:
+
+                 -1 + 4 * 6
+                 ( 35 + 5 ) % 7
+                 14 + -4 * 6 / 11
+                2 + 15 / 6 * 1 - 7 % 2
+            
+                2. Write a C# Sharp program that takes two numbers as input and performs an operation (+,-,*,x,/) on them and displays the result of that operation.
+
+                   Test Data
+                   Input first number: 20
+                   Input operation: -
+                   Input second number: 12
+                    Expected Output :
+                    20 - 12 = 8
+
+            3. Write a C# Sharp program that takes the radius of a circle as input and calculates the perimeter and area of the circle. 
+
+            4. Write a C# Sharp program to accept two integers and check whether they are equal or not. 
+
+            5. Write a C# Sharp program to display the cube of an integer up to given number.
+                Test Data :
+                Input number of terms : 5
+                Expected Output :
+                Number is : 1 and cube of the 1 is :1
+                Number is : 2 and cube of the 2 is :8
+                Number is : 3 and cube of the 3 is :27
+                Number is : 4 and cube of the 4 is :64
+                Number is : 5 and cube of the 5 is :125
+
+            Write Methods for each problem. Write a menu to run each problem.
+             */
+
+        }
+    }
+}
