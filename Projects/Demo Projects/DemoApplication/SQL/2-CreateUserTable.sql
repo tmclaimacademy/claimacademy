43bb78ee0c89972d69cba8cf8f801f586e1e9583
Tavish Misra <tavish.misra@outlook.com>
2024-10-25 18:57:10 -0500

    Add SQL database and table creation scripts to DemoApplication

---
+++ Projects/Demo Projects/DemoApplication/SQL/2-CreateUserTable.sql
@@ -0,0 +1,22 @@
+﻿USE [WebApp]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 10/25/2024 6:55:15 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [varchar](1000) NOT NULL,
+	[PasswordHash] [varchar](1000) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
---
+++ Projects/Demo Projects/DemoApplication/SQL/3-CreateUserSessionsTable.sql
@@ -0,0 +1,27 @@
+﻿USE [WebApp]
+GO
+
+/****** Object:  Table [dbo].[UserSessions]    Script Date: 10/25/2024 6:55:56 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[UserSessions](
+	[SessionID] [varchar](255) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[LastActivityDateTime] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[SessionID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[UserSessions]  WITH CHECK ADD FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
+GO
+
+
